aac245a879eb842f0c1ffcd800ef55bc26d45a2e
Natalie Lazarev <lazarev.n@yahoo.com>
2024-07-10 14:04:05 +0200

    first attempt

--- Main.java
+++ Main.java
@@ -1,3 +1,20 @@
-public class Main{
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Main main = new Main();
+
+        System.out.println("Please enter an integer: ");
+        int value = scanner.nextInt();
+
+        main.increment(value);
+        System.out.println("original value: " + value);
+
+    }
+
+    public void increment(int integer) {
+        integer++;
+        System.out.println("incremented value: " + integer);
+    }
 }
